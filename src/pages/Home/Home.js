1bd4f13d1ff5823a82db35cbd39634b8783c6453
Prashant Tripathi <90204816+ptripathi2002@users.noreply.github.com>
2024-05-05 01:13:45 +0530

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -44,16 +44,7 @@ const Home = () => {
             Inventory system to control and manage proucts in the warehouse in
             real timeand integrated to make it easier to develop your business.
           </p>
-          <div className="hero-buttons">
-            <button className="--btn --btn-secondary">
-              <Link to="/dashboard">Free Trial 1 Month</Link>
-            </button>
-          </div>
-          <div className="--flex-start">
-            <NumberText num="14K" text="Brand Owners" />
-            <NumberText num="23K" text="Active Users" />
-            <NumberText num="500+" text="Partners" />
-          </div>
+         
         </div>
 
         <div className="hero-image">
